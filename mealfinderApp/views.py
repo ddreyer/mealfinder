a9b8f2bad530f6c42b041d4ab1485662c4677afe
Dylan <ddreyer@berkeley.edu>
2017-08-14 23:54:01 -0700

    adding meal finding logic

--- mealfinderApp/views.py
+++ mealfinderApp/views.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import Restaurant
 from serializers import RestaurantSerializer
 
-@csrf_exempt
 def findMeal(request):
-    if request.method == "POST":
-        restaurant = Restaurant.objects.get(name=request.POST['restaurant'])
+    """ Finds meal based on POST request from client.
+
+    Arguments:
+    request -- should contain restaurant name and 
+            macros in a matrix (protein, fat, carbs)
+    """
+    if request.method == "GET":
+        restaurant = Restaurant.objects.get(name=request.GET['restaurant'])
+        clientMacros = JSON.serialize(request.GET['macros'])
+        # find closest meal to fit macros
+        for meal in restaurant.meals:
+
+            data = 
         return HttpResponse(restaurant.name)
+        # return Response()
     else:
         return HttpResponse("Invalid request")
